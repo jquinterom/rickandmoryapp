71b4fb3cb8850d03a5b058c893e3d79ca35636fe
jquinterom <jfredyquintero@hotmail.com>
2022-01-12 09:40:47 -0500

    Navigation success and new data item

--- app/src/main/java/com/example/rickandmortyapp/data/Item.kt
+++ app/src/main/java/com/example/rickandmortyapp/data/Item.kt
@@ -1,4 +1,13 @@
 package com.example.rickandmortyapp.data
 
-class Item {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item")
+data class Item(
+    @PrimaryKey()
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+    val name: String
+)
