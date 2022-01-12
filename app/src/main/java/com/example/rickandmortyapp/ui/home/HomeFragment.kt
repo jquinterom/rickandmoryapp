e39c03d35ef27e49226aeeaf690919b506eba0d1
jquinterom <jfredyquintero@hotmail.com>
2022-01-12 10:42:18 -0500

    First connection to API

--- app/src/main/java/com/example/rickandmortyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.rickandmortyapp.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.rickandmortyapp.R
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.rickandmortyapp.databinding.FragmentHomeBinding
+import com.example.rickandmortyapp.endPoints.EndPoint
+import com.example.rickandmortyapp.http.HttpSingleton
 
 class HomeFragment : Fragment() {
 
@@ -35,11 +39,33 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        getData()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getData(){
+        val url = EndPoint.CHARACTERS
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                //textView.text = "Response: %s".format(response.toString())
+                Log.d("response", response.toString())
+            },
+            { error ->
+                // TODO: Handle error
+                Log.e("error", error.toString())
+            }
+        )
+
+        // Access the RequestQueue through your singleton class.
+        HttpSingleton.getInstance(binding.root.context).addToRequestQueue(jsonObjectRequest)
+
     }
 }
